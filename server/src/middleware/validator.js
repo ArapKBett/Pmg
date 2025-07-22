e1198e7336a8f7013bd2e89d3e33ca025cf63e4e
Arap Bett <kipkorirjob@kabarak.ac.ke>
2025-07-22 18:28:38 +0300

    Create validator.js

---
+++ server/src/middleware/validator.js
@@ -0,0 +1,30 @@
+const { validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+
+// Request validation middleware
+exports.validate = (validations) => {
+  return async (req, res, next) => {
+    await Promise.all(validations.map(validation => validation.run(req)));
+
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+      return next();
+    }
+    res.status(400).json({ errors: errors.array() });
+  };
+};
+
+// Authentication middleware
+exports.authenticate = (req, res, next) => {
+  const token = req.headers.authorization?.split(' ')[1];
+  if (!token) {
+    return res.status(401).json({ error: 'Authentication required' });
+  }
+
+  try {
+    req.user = jwt.verify(token, process.env.JWT_SECRET);
+    next();
+  } catch (err) {
+    res.status(403).json({ error: 'Invalid token' });
+  }
+};
